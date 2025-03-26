8965eb6f3506f3d11284b2dbb58a28f153e2df0c
Chirag Jain <cjain8060@gmail.com>
2025-03-26 23:04:49 +0530

    Time: 23 ms (47.40%) | Memory: 47.5 MB (31.66%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,65 @@
+/*
+    * The minimum way a graph/ can be fully connected is through a spanning tree ( n nodes h aur n - 1 edges )
+    * Matlab n computers ko atleast n - 1 wires chaiye ( if this is not the case then -1 not possible )
+    * 
+*/
+class DisjointSet{
+public:
+    vector<int> parent;
+    vector<unsigned long long> size;
+
+    DisjointSet(int n){
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for(int i = 0; i <= n; i++){
+            parent[i] = i;
+        }
+    }
+
+    int findUltimateParent(int node){
+        if(parent[node] == node) return node;
+        parent[node] = findUltimateParent(parent[node]);
+        return parent[node];
+    }
+
+    void unionBySize(int u, int v){
+        int upu = findUltimateParent(u);
+        int upv = findUltimateParent(v);
+
+        // always connect smaller size to bigger one
+        if(size[upu] < size[upv]){
+            parent[upu] = upv;
+            size[upv] += size[upu];
+        } else {
+            parent[upv] = upu;
+            size[upu] += size[upv];
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int wires = connections.size();
+        if(wires < n - 1) return -1; // if we dont have atleast n - 1 wires then not possible
+
+        // using disjoint set to find number of componets
+        // according to spanning tree we need atleast n - 1 edges to make n nodes completely connected
+
+        DisjointSet ds(n);
+        int numberOfComponents = 0;
+
+        for(auto connection: connections){
+            int u = connection[0];
+            int v = connection[1];
+            ds.unionBySize(u, v);
+        }
+
+        for(int i = 0; i < n; i++) {
+            // if any node is there own parent then they form a component
+            if(ds.parent[i] == i) numberOfComponents++;
+        }
+
+        return numberOfComponents - 1;
+    }
+};
