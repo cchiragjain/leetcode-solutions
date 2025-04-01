7d94581aae3c023755ad75bdf0e53f7cc22ea909
Chirag Jain <cjain8060@gmail.com>
2025-04-01 16:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.31%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -14,12 +14,27 @@ private:
         dp[n] = solveUsingMem(n - 1, dp) + solveUsingMem(n - 2, dp);
         return dp[n];
     }
+
+    int solveUsingTabulation(int n){
+        if(n <= 1) return n; // base cases can causes issues if say n was 0 then dp will be of size 1 but we will accessing dp[1] etc.
+
+        vector<int> dp(n + 1, -1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2]; // building solutions using previous
+        }
+
+        return dp[n];
+    }
     
 public:
     int fib(int n) {
-        vector<int> dp(n + 1, -1); // need to return the nth fib number
+        // vector<int> dp(n + 1, -1); // need to return the nth fib number
         
-        int result = solveUsingMem(n, dp);
-        return result;    
+        // int result = solveUsingMem(n, dp);
+        return solveUsingTabulation(n);    
     }
 };
