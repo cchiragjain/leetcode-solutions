46e1bcb7630253cbc563b996b633555bd3702dfe
Chirag Jain <cjain8060@gmail.com>
2025-04-11 23:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (10.07%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -13,11 +13,31 @@ private:
 
         return memo[row][i];
     }
+
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> memo(n, vector<int>(n, -1));
+        // vector<vector<int>> memo(n, vector<int>(n, -1));
 
-        return minPathSum(0, 0, triangle, memo);
+        // return minPathSum(0, 0, triangle, memo);
+
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        
+        dp[0][0] = triangle[0][0];
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < triangle[i].size(); j++){
+                int topValue = dp[i - 1][j];
+                int topLeftValue = j > 0 ? dp[i - 1][j - 1] : INT_MAX;
+
+                dp[i][j] = triangle[i][j] + min(topValue, topLeftValue);
+            }
+        }
+
+        int minValue = INT_MAX;
+        for(int i = 0; i < n; i++){
+            minValue = min(minValue, dp[n - 1][i]);
+        }
+        return minValue;
     }
 };
