d1aa528d787d391eef5820e3d30d05e3e7ccd5a9
Chirag Jain <cjain8060@gmail.com>
2025-04-11 19:38:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (56.22%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -11,8 +11,18 @@ private:
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m + 1, vector<int>(n + 1, -1));
+        // vector<vector<int>> memo(m + 1, vector<int>(n + 1, -1));
+        
+        // return uniquePaths(m, n, memo);
 
-        return uniquePaths(m, n, memo);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 1));
+
+        for(int i = 2; i <= m; i++){
+            for(int j = 2; j <= n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m][n];
     }
 };
