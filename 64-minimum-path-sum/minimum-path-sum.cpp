c49b465a85fe63d161bd572f2cb59be0e7d167f9
Chirag Jain <cjain8060@gmail.com>
2025-04-11 20:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (79.85%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -15,10 +15,15 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        // vector<vector<int>> memo(m, vector<int>(n, -1));
+        /* memoisation
+        vector<vector<int>> memo(m, vector<int>(n, -1));
 
-        // return minPathSum(m - 1, n - 1, grid, memo);
-    
+        return minPathSum(m - 1, n - 1, grid, memo);
+        */
+
+        // tabulation
+        
+        /*
         vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
         dp[0][0] = grid[0][0];
 
@@ -32,7 +37,25 @@ public:
                 dp[i][j] = grid[i][j] + min(topValue, leftValue);
             }
         }
-
         return dp[m - 1][n - 1];
+        */
+
+        // can be further optimised if we see that only needs to store a cells top and left value but each cell top value will be from previous row
+        // do dry run 
+        vector<int> dp(n, -1);
+        dp[0] = grid[0][0];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n;j++){
+                if(i == 0 && j == 0) continue;
+
+                int topValue = i > 0 ? dp[j] : INT_MAX;
+                int leftValue = j > 0 ? dp[j - 1] : INT_MAX;
+
+                dp[j] = grid[i][j] + min(topValue, leftValue);
+            }
+        }
+
+        return dp[n - 1];
     }
 };
