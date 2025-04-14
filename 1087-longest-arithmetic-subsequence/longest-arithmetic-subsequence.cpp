7120a30b055efdafecccd29f1a8a9c83b966f534
Chirag Jain <cjain8060@gmail.com>
2025-04-14 20:04:41 +0530

    Time: 117 ms (84.79%) | Memory: 145.7 MB (78.02%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -21,16 +21,29 @@ public:
         if(n <= 2) return n; // any array like [1,2] say for example will have the length only as no other element to check with
 
         int result = 0;
-        // d can at max be 500
-        // if we want to avoid negative d we are adding a constant 501 to our result everytime
-        vector<vector<int>> memo(n, vector<int>(1003, -1));
+        // recursion + memoisation
+        // // d can at max be 500
+        // // if we want to avoid negative d we are adding a constant 501 to our result everytime
+        // vector<vector<int>> memo(n, vector<int>(1003, -1));
+
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i + 1; j < n; j++){
+        //         // check all i, j pairs with d difference in the left side of i if we can keep them as part of subsequence
+        // // can do for each idx and d as well but d can be negative
+        //         int d = nums[j] - nums[i];
+        //         result = max(result, 2 + solve(i, n, nums, d, memo));
+        //     }
+        // }
+
+        // return result;
+        vector<vector<int>> dp(n, vector<int>(1003, -1));
 
         for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                // check all i, j pairs with d difference in the left side of i if we can keep them as part of subsequence
-        // can do for each idx and d as well but d can be negative
-                int d = nums[j] - nums[i];
-                result = max(result, 2 + solve(i, n, nums, d, memo));
+            for(int j = 0; j < i; j++){
+                int diff = nums[i] - nums[j] + 501;
+
+                dp[i][diff] = dp[j][diff] > 0 ? 1 + dp[j][diff] : 2; // i or j will form a subsequence or if j already was part of one then attach i to it as well
+                result = max(result, dp[i][diff]);
             }
         }
 
