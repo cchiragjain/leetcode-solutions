496d3213d3525faa0b5b78696f46012d4e86be17
Chirag Jain <cjain8060@gmail.com>
2025-04-14 13:30:15 +0530

    Time: 8 ms (46.77%) | Memory: 13 MB (73.79%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -24,8 +24,39 @@ public:
     int minDistance(string word1, string word2) {
         int m = word1.length();
         int n = word2.length();
-        vector<vector<int>> memo(m, vector<int>(n, -1));
+        // memoisation       
+        // vector<vector<int>> memo(m, vector<int>(n, -1));
 
-        return solve(0, 0, m, n, word1, word2, memo);
+        // return solve(0, 0, m, n, word1, word2, memo);
+    
+        // dp[i][j] represents for string1 length i and string2 length j min operations required to convert string1 to string2
+        // ex. dp[0][2] will be 2 ex. "" and "ab" need 2 operations to convert "" to ab
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for(int i = 0; i <= m; i++){
+            dp[i][0] = i; // to convert 2 to 0 will require 2 delete operations
+        }
+        for(int j = 0; j <= n; j++){
+            dp[0][j] = j;
+        }
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i - 1][j -1]; // same letter no operation is required
+                } else {
+                    int insertCost = 1 + dp[i][j - 1];
+                    int deleteCost = 1 + dp[i - 1][j];
+                    int replaceCost = 1 + dp[i - 1][j - 1];
+
+                    dp[i][j] = min(insertCost, min(
+                        deleteCost,
+                        replaceCost
+                    ));
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
