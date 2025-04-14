11e0284231adc88e7236d7c9b3aa0da4e6da5f78
Chirag Jain <cjain8060@gmail.com>
2025-04-14 23:45:24 +0530

    Time: 5 ms (67.56%) | Memory: 14.1 MB (80.12%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -19,7 +19,24 @@ public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
-        memset(dp, -1, sizeof dp);
-        return solve(0, 0, m, n, nums1, nums2);
+        // recursion + memoisation m * n
+        // memset(dp, -1, sizeof(dp));
+        // return solve(0, 0, m, n, nums1, nums2);
+    
+        memset(dp, 0, sizeof(dp));
+        // its better to use vector though
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(nums1[i - 1] == nums2[j - 1]){
+                    // ex 1 2 vs 3 2
+                    // 2 and 2 are equal now check how many possible for 1 and 3
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
