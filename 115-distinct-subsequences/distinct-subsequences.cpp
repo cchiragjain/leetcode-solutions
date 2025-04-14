381db94ab661c385cb957002fb7c00438c915feb
Chirag Jain <cjain8060@gmail.com>
2025-04-14 14:54:52 +0530

    Time: 26 ms (74.27%) | Memory: 44.2 MB (40.17%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,3 +1,4 @@
+#define ull unsigned long long
 class Solution {
 private:
     int solve(int i, int j, int m, int n, string& s1, string& s2, vector<vector<int>>& memo){
@@ -25,8 +26,32 @@ public:
         int m = s.length();
         int n = t.length();
 
-        vector<vector<int>> memo(m, vector<int>(n, -1));
+        // O(m * n) memoisation
+        // vector<vector<int>> memo(m, vector<int>(n, -1));
 
-        return solve(0, 0, m, n, s, t, memo);
+        // return solve(0, 0, m, n, s, t, memo);
+    
+        vector<vector<ull>> dp(m + 1, vector<ull>(n + 1, -1));
+        dp[0][0] = 1;
+
+        for(int i = 1; i <= m; i++){
+            dp[i][0] = 1; 
+        }
+
+        for(int j = 1; j <= n; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s[i - 1] == t[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return (int)dp[m][n];
     }
 };
