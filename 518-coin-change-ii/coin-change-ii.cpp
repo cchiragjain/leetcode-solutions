d1c2a4e39d78a4c5806d26d451c3888e78d31144
Chirag Jain <cjain8060@gmail.com>
2025-04-15 19:54:08 +0530

    Time: 28 ms (32.94%) | Memory: 46.5 MB (10.52%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -20,11 +20,41 @@ private:
 
 public:
     int change(int amount, vector<int>& coins) {
-        // recursion is ( 2 ^ n + amount)
         int n = coins.size();
 
-        vector<vector<int>> memo(n, vector<int>(amount + 1, -1));
+        // recursion is ( 2 ^ n + amount)
+        // memoisation is N * amount
+        // vector<vector<int>> memo(n, vector<int>(amount + 1, -1));
 
-        return solve(0, amount, coins, memo);
+        // return solve(0, amount, coins, memo);
+        // dp[i][j] will give for i - 1 th index array number of ways to get j amount
+
+        vector<vector<unsigned long long>> dp(n + 1, vector<unsigned long long>(amount + 1, 0));
+        
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 1;
+             // there is one way to use n coins for 0 amount
+             // do nothing
+        }
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= amount; j++){
+                unsigned long long take = 0;
+
+                if(coins[i - 1] <= j){
+                    // take what ever ways we can do for
+                    // this many coins and less amount
+                    // can use same coin mulitple times
+                    take = dp[i][j - coins[i - 1]];
+                }
+
+                // dont use this coin and check how many ways for before coins with the same amount
+                unsigned long long notTake = dp[i - 1][j];
+
+                dp[i][j] = take + notTake;
+            }
+        }
+
+        return dp[n][amount];
     }
 };
