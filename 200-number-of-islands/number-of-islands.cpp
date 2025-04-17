9cdace0f6c9ad8cf543e89f502c0e3dd2b4fbc22
Chirag Jain <cjain8060@gmail.com>
2025-04-17 22:40:17 +0530

    Time: 32 ms (29.62%) | Memory: 24.1 MB (24.12%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,46 +1,47 @@
 class Solution {
-    private:
-        void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<bool>>& visited){
-            int m = grid.size();
-            int n = grid[0].size();
-            
-            queue<pair<int, int>> q;
-            visited[row][col] = true;
-            
-            q.push({row, col});
-            vector<pair<int, int>> directions = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
-            
-            while(!q.empty()){
-                auto [row, col] = q.front();
-                q.pop();
+private:
+    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col, int m, int n){
+        queue<pair<int, int>> q;
+        visited[row][col] = true;
 
-                for(auto& [dy, dx]: directions){
-                    int newRow = row + dy;
-                    int newCol = col + dx;
+        q.push({row, col});
 
-                    if(newRow < m && newRow >= 0 && newCol < n && newCol >= 0 && grid[newRow][newCol] == '1' && !visited[newRow][newCol]){
-                        visited[newRow][newCol] = true;
-                        q.push({newRow, newCol});
-                    }
+        vector<pair<int, int>> directions = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
+
+        while(!q.empty()){
+            auto [row, col] = q.front();
+            q.pop();
+
+            for(auto& [dy, dx]: directions){
+                int newRow = row + dy;
+                int newCol = col + dx;
+
+                if(newRow < m && newRow >= 0 && newCol < n && newCol >= 0 && grid[newRow][newCol] == '1' && !visited[newRow][newCol]){
+                    visited[newRow][newCol] = true;
+                    q.push({newRow, newCol});
                 }
             }
-
         }
+    }
 
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
         int m = grid.size();
         int n = grid[0].size();
 
         vector<vector<bool>> visited(m, vector<bool>(n, false));
 
+        int count = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == '1' && !visited[i][j]){
-                    visited[i][j] = true;
-                    bfs(i, j, grid, visited);
+                if(!visited[i][j] && grid[i][j] == '1'){
                     count++;
+                    bfs(grid, visited, i, j, m, n);
                 }
             }
         }
