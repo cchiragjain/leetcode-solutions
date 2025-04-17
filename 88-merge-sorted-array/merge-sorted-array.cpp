ad6a078127f5769bfbbf22a76bf56684493bea34
Chirag Jain <cjain8060@gmail.com>
2025-04-17 19:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (39.11%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,32 +1,57 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> numsCopy;
-        int i = 0;
-        int j = 0;
+        // better approach
+        int i = m - 1;
+        int j = n - 1;
 
-        while(i < m && j < n){
+        int toInsertIdx = nums1.size() - 1;
+
+        while(i >= 0 && j >= 0){
             if(nums1[i] > nums2[j]){
-                numsCopy.push_back(nums2[j]);
-                j++;
-            } else {
-                numsCopy.push_back(nums1[i]);
-                i++;
+                nums1[toInsertIdx] = nums1[i];
+                i--;
+            } else{
+                nums1[toInsertIdx] = nums2[j];
+                j--;
             }
+            toInsertIdx--;
         }
 
-        while(i < m){
-            numsCopy.push_back(nums1[i]);
-            i++;
+        while(j >= 0) {
+            nums1[toInsertIdx] = nums2[j];
+            j--;
+            toInsertIdx--;
         }
+        
+        // brute force approach uses extra space
+        // T.C => O(M + N) S.C => O(M + N)
+        // vector<int> numsCopy;
+        // int i = 0;
+        // int j = 0;
 
-        while(j < n){
-            numsCopy.push_back(nums2[j]);
-            j++;
-        }
+        // while(i < m && j < n){
+        //     if(nums1[i] > nums2[j]){
+        //         numsCopy.push_back(nums2[j]);
+        //         j++;
+        //     } else {
+        //         numsCopy.push_back(nums1[i]);
+        //         i++;
+        //     }
+        // }
 
-        for(int i = 0; i < numsCopy.size(); i++){
-            nums1[i] = numsCopy[i];
-        }
+        // while(i < m){
+        //     numsCopy.push_back(nums1[i]);
+        //     i++;
+        // }
+
+        // while(j < n){
+        //     numsCopy.push_back(nums2[j]);
+        //     j++;
+        // }
+
+        // for(int i = 0; i < numsCopy.size(); i++){
+        //     nums1[i] = numsCopy[i];
+        // }
     }
 };
