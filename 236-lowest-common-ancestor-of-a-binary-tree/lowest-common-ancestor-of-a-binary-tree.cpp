3887c9deb57a1d8ca2a5d5f74ec9db79816c4a59
Chirag Jain <cjain8060@gmail.com>
2025-04-18 00:16:21 +0530

    Time: 16 ms (27.55%) | Memory: 17.3 MB (67.91%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -24,21 +24,34 @@ private:
     }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root -> left, p, q);
+        TreeNode* right = lowestCommonAncestor(root -> right, p, q);
+
+        // if using this root p and q both were found then that means this is the lowest common 
+        if(left && right) return root;
+        if(left && !right) return left; // take second one as example if we are standing at 3 and found 5 in left and in the right call were not able to find 4 then 5 is lca only since 4 will be in some part in left
+        if(right && !left) return right;
+
+        return nullptr;
+        
         // brute force approach
+        // it is T.C => O(N) but uses two traversals and extra space to store
+
         // get the path from root to p and q nodes
         // then check the last element that is equal
+        // vector<TreeNode*> pPath;
+        // vector<TreeNode*> qPath;
 
-        vector<TreeNode*> pPath;
-        vector<TreeNode*> qPath;
+        // findPath(root, p, pPath);
+        // findPath(root, q, qPath);
 
-        findPath(root, p, pPath);
-        findPath(root, q, qPath);
+        // int i = 0;
+        // while(i < pPath.size() && i < qPath.size() && pPath[i] == qPath[i]){
+        //     i++;
+        // }
 
-        int i = 0;
-        while(i < pPath.size() && i < qPath.size() && pPath[i] == qPath[i]){
-            i++;
-        }
-
-        return pPath[i - 1];
+        // return pPath[i - 1];
     }
 };
